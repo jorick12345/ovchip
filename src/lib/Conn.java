c0ffe909a73a8979aa3859387144210b12cc4c32
super <jorick.vanderhave@student.hu.nl>
2022-10-11 15:32:14 +0200

    p4 update

--- src/lib/Conn.java
+++ src/lib/Conn.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class Conn {
     public static Connection getConnection(){
         try {
-            return DriverManager.getConnection("jdbc:postgresql://localhost/ovchip?user=postgres&password=jaloe43287990");
+            return DriverManager.getConnection("jdbc:postgresql://localhost/ovchip?user=postgres&password=postgres");
         } catch (SQLException ex) {
             throw new RuntimeException("Error: RuntimeException",ex);
         }
